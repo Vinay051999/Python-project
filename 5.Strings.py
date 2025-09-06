70cc36a8f255e5002585bfef9d85b66a97b9f363
Vinay G A <vinayreddyga1999@gmail.com>
2025-09-06 19:51:39 +0530

    Second Commit

---
+++ 5.Strings.py
@@ -0,0 +1,10 @@
+String1="Hello, World!" # String variable
+String2='Python is fun' # String variable
+
+String3="""This is a multi-line 
+string in Python""" # Multi-line string variable
+String4="This is a string with a newline character\n See the new line" # String with escape character
+print(String1) #printing String1
+print(String2) #printing String2
+print(String3) #printing String3
+print(String4) #printing String4
