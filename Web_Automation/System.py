bb66a70ff469e75e5f03ac558d593de9d3d06026
Vinay G A <vinayreddyga1999@gmail.com>
2025-09-07 16:37:56 +0530

    Sys

---
+++ Web_Automation/System.py
@@ -0,0 +1,9 @@
+import sys
+print("Python version", sys.version)  # Print the Python version
+print("python Executable path", sys.executable)  # Print the path of the Python executable
+print("System platform", sys.platform)  # Print the platform information
+#print("Module Search path", sys.path)  # Print the list of directories Python searches for modules
+print("file name", sys.argv[0])  # Print the name of the script being executed
+
+# exit the program
+sys.exit()  # Exit the program
