881c3ce0ba567d136ed6826d9ad2c1004996a477
Vinay G A <vinayreddyga1999@gmail.com>
2025-09-09 23:53:14 +0530

    functions

---
+++ Function_and_Recursion/print_the_list.py
@@ -0,0 +1,7 @@
+cities = ["bangalore", "mumbai", "delhi", "kolkata"]
+movies = ["avatar", "inception", "interstellar", "gravity"]
+def print_the_list(list):
+    print(len(list))
+
+print_the_list(cities)
+print_the_list(movies)
